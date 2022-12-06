d2de414b34b40ffe3b9e587501cb0526658f3d72
chenjie <1490478630@qq.com>
2022-12-06 14:37:47 +0800

    feat:toRefs,proxyRefs

--- vue_design_and_practice/code/06/reactivity-for-primitive.js
+++ vue_design_and_practice/code/06/reactivity-for-primitive.js
@@ -1,13 +1,42 @@
 // 原始值的响应式
-import { ref, toRef } from "../../utils/ref.js"
+import { ref, toRef, toRefs, proxyRefs } from "../../utils/ref.js"
 import { effect, reactive } from "../../utils/reactive-all.js";
-const obj = reactive({ foo:1 })
-// 原始值的响应方案
-const refVal = ref(1);
-effect(() => {
-    console.log("原始值的响应式", refVal.value)
-})
+const obj = reactive({ foo: 1, bar: 2 })
+
 // ref
+// const refVal = ref(1);
+// effect(() => {
+//     console.log("原始值的响应式", refVal.value)
+// })
+// setTimeout(() => {
+//     refVal.value = 2
+// }, 1000)
+
+// toRef: 解决响应丢失问题
+const newObj = {
+    foo: toRef(obj, 'foo'),
+    bar: toRef(obj, 'bar')
+}
+
+effect(() => {
+    console.log("toRef解决响应丢失问题", newObj.foo.value)
+})
+
 setTimeout(() => {
-    refVal.value = 2
+    newObj.foo.value++
 }, 1000)
+
+// toRefs: 批量处理响应式对象, 返回ref类型得的响应式数据
+const newObj1 = { ...toRefs(obj)}
+
+effect(() => {
+    console.log("toRefs: 批量处理响应式对象, 返回ref类型得的响应式数据", newObj1.bar.value)
+})
+
+setTimeout(() => {
+    newObj1.bar.value++
+}, 1000)
+
+// 自动脱ref
+const newObj2 = proxyRefs({...toRefs(obj)})
+console.log("自动脱ref", newObj2.foo)
